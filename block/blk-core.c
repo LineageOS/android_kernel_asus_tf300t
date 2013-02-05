2f9e4fff47ff0c2b61e9a44ddabf2ba64acef6ad
Pedro Carneiro <pcarneiro10@gmail.com>
2013-02-05 17:41:02 +0100

    block: fixes required to make the kernel compile with ROW.
    Change-Id: Ibccfe5b295e1f97e9a2ea11abb30cd90f5968d70
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -302,7 +302,18 @@ void __blk_run_queue(struct request_queue *q)
 	if (unlikely(blk_queue_stopped(q)))
 		return;
 
+<<<<<<< HEAD
 	q->request_fn(q);
+=======
+	if (!q->notified_urgent &&
+		q->elevator->elevator_type->ops.elevator_is_urgent_fn &&
+		q->urgent_request_fn &&
+		q->elevator->elevator_type->ops.elevator_is_urgent_fn(q)) {
+		q->notified_urgent = true;
+		q->urgent_request_fn(q);
+	} else
+		q->request_fn(q);
+>>>>>>> 6348561... block: fixes required to make the kernel compile with ROW.
 }
 EXPORT_SYMBOL(__blk_run_queue);
 
@@ -929,6 +940,53 @@ void blk_requeue_request(struct request_queue *q, struct request *rq)
 }
 EXPORT_SYMBOL(blk_requeue_request);
 
+<<<<<<< HEAD
+=======
+/**
+ * blk_reinsert_request() - Insert a request back to the scheduler
+ * @q:		request queue
+ * @rq:		request to be inserted
+ *
+ * This function inserts the request back to the scheduler as if
+ * it was never dispatched.
+ *
+ * Return: 0 on success, error code on fail
+ */
+int blk_reinsert_request(struct request_queue *q, struct request *rq)
+{
+	if (unlikely(!rq) || unlikely(!q))
+		return -EIO;
+
+	blk_delete_timer(rq);
+	blk_clear_rq_complete(rq);
+	trace_block_rq_requeue(q, rq);
+
+	if (blk_rq_tagged(rq))
+		blk_queue_end_tag(q, rq);
+
+	BUG_ON(blk_queued_rq(rq));
+
+	return elv_reinsert_request(q, rq);
+}
+EXPORT_SYMBOL(blk_reinsert_request);
+
+/**
+ * blk_reinsert_req_sup() - check whether the scheduler supports
+ *          reinsertion of requests
+ * @q:		request queue
+ *
+ * Returns true if the current scheduler supports reinserting
+ * request. False otherwise
+ */
+bool blk_reinsert_req_sup(struct request_queue *q)
+{
+	if (unlikely(!q))
+		return false;
+	return q->elevator->elevator_type->ops.elevator_reinsert_req_fn ? true : false;
+}
+EXPORT_SYMBOL(blk_reinsert_req_sup);
+
+>>>>>>> 6348561... block: fixes required to make the kernel compile with ROW.
 static void add_acct_request(struct request_queue *q, struct request *rq,
 			     int where)
 {
